5fcd7f572bf2f7051c383fb4f3314f0a4c291137
hoverslam <hofmann.christopher@outlook.com>
2022-01-04 18:14:46 +0100

    add player colors

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Tic-Tac-Toe
 An implementation of the simple game "Tic-Tac-Toe" that further can be used to train an RL agent. At the moment it has an "AI" called Karen that choose actions randomly.
 
+## Prerequisits
+* Python v3.10.1
+* Colorama v0.4.4
+* Termcolor v1.1.0
+
 ## How To
 * Run play.py
 * Select from the available options/positions and press enter. Currently there are no checks on user inputs.
+
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,25 +1,42 @@
 import random
+from colorama import init
+from termcolor import colored
 
 class Board:
     """ The board on which the game is played. """
     
     def __init__(self):
-        self.state = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+        self.state = [7, 8, 9, 4, 5, 6, 1, 2, 3]
         
     def available_postions(self):
         p = []
-        for index, key in enumerate(self.state):
-            if key == "-" : p.append(index)            
+        for k, v in enumerate(self.state):
+            if isinstance(v, int) : p.append(k)            
         return p
     
     def update_state(self, player, position):
         self.state[position] = player
+        
+    def colored_players(self):
+        colors = self.state.copy()
+        for k, v in enumerate(self.state):
+            if v == "X" : colors[k] = "green"
+            elif v == "O" : colors[k] = "red"
+            else : colors[k] = "white"
+            
+        return colors            
     
     def show_board(self):
-        print("{} {} {}\n{} {} {}\n{} {} {}".format(
-            self.state[0], self.state[1], self.state[2],
-            self.state[3], self.state[4], self.state[5],
-            self.state[6], self.state[7], self.state[8])) 
+        colors = self.colored_players()
+        print(colored(self.state[0], colors[0]) + " " + 
+              colored(self.state[1], colors[1]) + " " + 
+              colored(self.state[2], colors[2]) + "\n" + 
+              colored(self.state[3], colors[3]) + " " + 
+              colored(self.state[4], colors[4]) + " " + 
+              colored(self.state[5], colors[5]) + "\n" + 
+              colored(self.state[6], colors[6]) + " " + 
+              colored(self.state[7], colors[7]) + " " + 
+              colored(self.state[8], colors[8]))
     
 class Game:
     """ The logic to play the game. """
@@ -60,6 +77,21 @@ class Game:
         action = random.choice(positions) 
         return action
     
+    def convert_input(self, input):
+        action = None        
+        if input == 7 : action = 0
+        elif input == 8 : action = 1
+        elif input == 9 : action = 2
+        elif input == 4 : action = 3
+        elif input == 5 : action = 4
+        elif input == 6 : action = 5
+        elif input == 1 : action = 6
+        elif input == 2 : action = 7
+        elif input == 3 : action = 8
+        
+        return action
+            
+    
     def play_round(self, player, action):
         self.b.update_state(player, action)
         actions = self.b.available_postions()
@@ -68,10 +100,11 @@ class Game:
         
         return (state, actions, done) 
 
-    def play_game(self):      
-        actions = self.b.available_postions()
+    def play_game(self):
+        init()  # use colorama to make termcolor work on Windows too     
+        actions = self.b.available_postions()        
         ai = random.choice(["X", "O"])
-        current_player = "X"    
+        current_player = "X" 
         done = False
         i = 1
         
@@ -79,6 +112,7 @@ class Game:
         
         while done == False:
             print("### ROUND {}: Player '{}' ###".format(i, current_player))
+            self.b.show_board() 
             if self.players == "Karen" and current_player == ai:
                 action = self.karen_ai(actions)
             elif self.players == "God" and current_player == ai:
@@ -86,10 +120,10 @@ class Game:
             elif self.players == "Training":
                 action = self.karen_ai(actions)
             else:
-                action = int(input("Select position: {} ".format(actions))) 
+                action = int(input("Select position: "))
+                action = self.convert_input(action)
             
             state, actions, done = self.play_round(current_player, action)
-            self.b.show_board()
             print("")      
             
             i = i + 1 
@@ -97,7 +131,10 @@ class Game:
                 current_player = "O"
             else:
                 current_player = "X" 
-            
+             
+        self.b.show_board()
+        print("")  
+        
         if done == "Draw":
             print("###################")    
             print("!!! IT'S A DRAW !!!")
@@ -106,8 +143,5 @@ class Game:
             print("############################")    
             print("!!! WINNER IS PLAYER '{}' !!!".format(done))
             print("############################")
-     
-
-    # [TODO] GUI -> pygame 
-
- 
+  
+    # [TODO] pygame GUI (maybe) 
